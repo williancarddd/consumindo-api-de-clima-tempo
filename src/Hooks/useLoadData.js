e2a55e4990dc1a9c4f7a2fe78c269d7907b8bd2c
William Cardoso <williancard123@gmail.com>
2021-09-10 09:23:08 -0400

    alter query consult

--- src/Hooks/useLoadData.js
+++ src/Hooks/useLoadData.js
@@ -1,16 +1,19 @@
 import {api} from '../services/api'
 export function useLoadData(useEffect, dispatch, types_context_weather){
+
   useEffect(()=>{
     load_data()
     console.log('ok useLoad')
  }, [])
+
  async function load_data(){
    const response = await api.get('current?access_key=5180ad1c324597b8a0c1beb9c4c0a8cb',
    {params:{
-     query: 'Porto velho-ro'
+     query: 'Porto velho-Ro'
    }})
    const data = response.data
    console.log(data)
   dispatch({type: types_context_weather.LOAD_DATA, payload: data})
  }
+
 }
